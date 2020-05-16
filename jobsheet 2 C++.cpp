24dbc232dc8d79df99da96ce7288ed805ddad9ae
M. Rifyal Ka'Bah <64625156+mrifyal61@users.noreply.github.com>
2020-05-17 03:27:40 +0800

    Create jobsheet 2 C++.cpp
    Mengisi dan Menampilkan elemen array (dinamis)

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"M.Rifyal Ka'Bah_F1B019089'(19)\n\n";
+	int nim[5];
+	cout<<"Masukkan akhiran NIM : ";
+	cin>>nim[0];
+	cout<<"Masukkan akhiran NIM : ";
+	cin>>nim[1];
+	cout<<"Masukkan akhiran NIM : ";
+	cin>>nim[2];
+	cout<<"Masukkan akhiran NIM : ";
+	cin>>nim[3];
+	cout<<"Masukkan akhiran NIM : ";
+	cin>>nim[4];
+	cout<<endl;
+	cout<<"Akhiran NIM : "<<nim[0]<<endl;
+	cout<<"Akhiran NIM : "<<nim[1]<<endl;
+	cout<<"Akhiran NIM : "<<nim[2]<<endl;
+	cout<<"Akhiran NIM : "<<nim[3]<<endl;
+	cout<<"Akhiran NIM : "<<nim[4]<<endl;
+}
