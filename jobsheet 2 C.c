e4cd7398e7599edd9677f09bfffa3335ad2e93b6
M. Rifyal Ka'Bah <64625156+mrifyal61@users.noreply.github.com>
2020-05-17 03:26:54 +0800

    Create jobsheet 2 C.c
    Mengisi dan Menampilkan elemen array (dinamis)

---
+++ jobsheet 2 C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+main(){
+	printf("M.Rifyal Ka'Bah_F1B019089'(19)\n\n");
+	int nim[5];
+	printf("Masukkan akhiran NIM :  ");
+	scanf("%i",&nim[0]);
+		printf("Masukkan akhiran NIM :  ");
+	scanf("%i",&nim[1]);
+		printf("Masukkan akhiran NIM :  ");
+	scanf("%i",&nim[2]);
+		printf("Masukkan akhiran NIM :  ");
+	scanf("%i",&nim[3]);
+		printf("Masukkan akhiran NIM :  ");
+	scanf("%i",&nim[4]);
+	printf("Akhiran NIM :  %i\n",nim[0]);
+	printf("Akhiran NIM :  %i\n",nim[1]);
+	printf("Akhiran NIM :  %i\n",nim[2]);
+	printf("Akhiran NIM :  %i\n",nim[3]);
+	printf("Akhiran NIM :  %i\n",nim[4]);
+}
