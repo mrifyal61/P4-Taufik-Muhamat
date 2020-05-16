a63f9555cb51110c6c1acfe26dbd1b03ec01915c
M. Rifyal Ka'Bah <64625156+mrifyal61@users.noreply.github.com>
2020-05-17 03:23:14 +0800

    Create Joobsheet 1 C.c
    Menampilkan array satu dimensi

---
+++ Joobsheet 1 C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main(){
+printf("M.Rifyal Ka'Bah_F1B019089'(19)\n\n");
+int nim[6] = {89,90,91,89,88,87};
+
+	printf("Data ke-1 =  %d\n",nim[0]);
+	printf("Data ke-2 =  %d\n",nim[1]);
+	printf("Data ke-3 =  %d\n",nim[2]);
+	printf("Data ke-4 =  %d\n",nim[3]);
+	printf("Data ke-5 =  %d\n",nim[4]);
+	printf("Data ke-6 =  %d\n",nim[5]);
+	
+	
+}
